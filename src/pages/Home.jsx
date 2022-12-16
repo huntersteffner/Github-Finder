fa45f0aaebcf31476181e872b56abc049bbe89dd
huntersteffner <huntersteffner@gmail.com>
2022-12-16 14:48:29 -0500

    Created user results components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from '../components/users/UserResults'
+
+const Home = () => {
+    return (
+        <>
+        {/* Search component */}
+            <UserResults/>
+        </>
+    )
+}
+
+export default Home
